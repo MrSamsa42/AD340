5d43f561af67a52d07c65395cefeac151b3b9d42
Jonathan Adler <jradler@gmail.com>
2018-04-16 18:11:27 -0700

    Modified layout of Main activty and added a text field.  Added second activity and linked the two.

--- AdlerAndroidApp/app/src/main/java/com/example/mrsam/adlerandroidapp/MainActivity.java
+++ AdlerAndroidApp/app/src/main/java/com/example/mrsam/adlerandroidapp/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.mrsam.adlerandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static final String TAG = "mrsam.adlerandroidapp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate called from Main Activity");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called from Main Activity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called from Main Activity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called from Main Activity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called from Main Activity");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart called from Main Activity");
+    }
+
+    public void onSendQuestion(View view){
+        EditText questionView = (EditText) findViewById(R.id.question);
+        String questionText = questionView.getText().toString();
+
+        Intent intent = new Intent(this, ReceiveQuestion.class);
+        intent.putExtra("question", questionText);
+        startActivity(intent);
     }
 }
---
+++ AdlerAndroidApp/app/src/main/java/com/example/mrsam/adlerandroidapp/ReceiveQuestion.java
@@ -0,0 +1,53 @@
+package com.example.mrsam.adlerandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.TextView;
+
+public class ReceiveQuestion extends AppCompatActivity {
+
+    protected static final String TAG = "mrsam.adlerandroidapp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_question);
+        Intent intent = getIntent();
+        String questionText = intent.getStringExtra("question");
+        TextView questionView = (TextView)findViewById(R.id.question_received);
+        questionView.setText(questionText);
+        Log.d(TAG, "onCreate called from ReceiveQuestion");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called from ReceiveQuestion");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called from ReceiveQuestion");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called from ReceiveQuestion");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called from ReceiveQuestion");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart called from ReceiveQuestion");
+    }
+}
