b2947e934bd54392e9a289ba7b82dd64c7b986d0
Jonathan Adler <jradler@gmail.com>
2018-04-29 12:26:25 -0700

    All functionality for hw4 in place --  only styling remains

--- AdlerAndroidApp/app/src/main/java/com/example/mrsam/adlerandroidapp/MainActivity.java
+++ AdlerAndroidApp/app/src/main/java/com/example/mrsam/adlerandroidapp/MainActivity.java
@@ -4,12 +4,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -63,6 +63,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+    //Create options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        CharSequence text = "Settings!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+
+        return super.onOptionsItemSelected(item);
+    }
 
     public void onSendQuestion(View view){
         EditText questionView = (EditText) findViewById(R.id.question);
